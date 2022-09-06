4e05042731d84dd9985de3d53c29c614cba2cc4d
worawut srimala <worawut48@hotmail.co.th>
2022-09-06 18:22:23 +0700

    init

---
+++ src/pages/page_about/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About: React.FC = () => {
+    return (
+        <div className='mx-5'>
+            <div className='text-red-500'>
+                page_about
+            </div>
+        </div>
+    )
+}
+export default About
---
+++ src/pages/page_home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <div className='mx-5'>
+            <div className='text-red-500'>
+                page_home
+            </div>
+        </div>
+    )
+}
+export default Home
